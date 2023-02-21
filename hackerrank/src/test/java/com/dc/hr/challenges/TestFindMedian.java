8bcc26bac2a1d9d33755a5f57b1a8d4538837541
dchaika <dmitriy.chaika@gmail.com>
2023-02-21 19:08:50 +0400

    Add tests for FindMedian.java

---
+++ hackerrank/src/test/java/com/dc/hr/challenges/TestFindMedian.java
@@ -0,0 +1,14 @@
+package com.dc.hr.challenges;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class TestFindMedian {
+
+    @Test
+    void testCase0() {
+        Assertions.assertEquals(3, FindMedian.findMedian(Arrays.asList(0, 1, 2, 4, 6, 5, 3)));
+    }
+}
