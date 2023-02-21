c501305489fa3f7c78f0dfac691d6a1dc6838f70
dchaika <dmitriy.chaika@gmail.com>
2023-02-21 19:08:30 +0400

    Add solution for 'Find the Median'

---
+++ hackerrank/src/main/java/com/dc/hr/challenges/FindMedian.java
@@ -0,0 +1,26 @@
+package com.dc.hr.challenges;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FindMedian {
+
+    /*
+     * The median of a list of numbers is essentially its middle element after sorting.
+     * The same number of elements occur after it as before.
+     * Given a list of numbers with an odd number of elements, find the median. https://en.wikipedia.org/wiki/Median
+     *
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int findMedian(List<Integer> arr) {
+        return arr.stream()
+                .sorted()
+                .collect(Collectors.toList())
+                .subList(arr.size() / 2, arr.size() / 2 + 1)
+                .get(0);
+    }
+}
