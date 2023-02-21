fedbdb0fcdf5a86cee5e2615d37143309a6c3555
dchaika <dmitriy.chaika@gmail.com>
2023-02-21 13:55:18 +0400

    Add tests for MinimumBribes.class

---
+++ hackerrank/src/test/java/com/dc/hr/challenges/TestMinimumBribes.java
@@ -0,0 +1,55 @@
+package com.dc.hr.challenges;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+
+public class TestMinimumBribes {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    void testCase1_1() {
+        MinimumBribes.minimumBribes(Arrays.asList(2, 1, 5, 3, 4));
+        Assertions.assertEquals("3", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void testCase1_2() {
+        MinimumBribes.minimumBribes(Arrays.asList(2, 5, 1, 3, 4));
+        Assertions.assertEquals("Too chaotic", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void testCase2_1() {
+        MinimumBribes.minimumBribes(Arrays.asList(1, 2, 5, 3, 7, 8, 6, 4));
+        Assertions.assertEquals("7", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void testCase2_2() {
+        MinimumBribes.minimumBribes(Arrays.asList(5, 1, 2, 3, 7, 8, 6, 4));
+        Assertions.assertEquals("Too chaotic", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void testCase3() {
+        MinimumBribes.minimumBribes(Arrays.asList(1, 2, 5, 3, 4, 7, 8, 6));
+        Assertions.assertEquals("4", outputStreamCaptor.toString().trim());
+    }
+}
